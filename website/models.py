006b1eb26d13a213622a0b5d41bac0dbe77869c0
Nicholas Quirk <n11321814@qut.edu.au>
2026-08-30 13:38:12 +1000

    Fix Models Error

--- website/models.py
+++ website/models.py
@@ -3,9 +3,13 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
 
 class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
 
 class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
