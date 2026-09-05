8ed47b6bbb421ca72049f96ae0820fd3400c1dcf
Hugo Van Roy <jvanroy01@gmail.com>
2026-09-05 14:29:27 +1000

    Added register and login

--- website/auth.py
+++ website/auth.py
@@ -15,20 +15,51 @@ def login():
     login_form = LoginForm()
     error = None
     if login_form.validate_on_submit():
-        user_name = login_form.user_name.data
+        email = login_form.email.data
         password = login_form.password.data
-        user = db.session.scalar(db.select(User).where(User.name==user_name))
+        user = db.session.scalar(db.select(User).where(User.email==email))
         if user is None:
-            error = 'Incorrect user name'
+            error = 'Incorrect email'
         elif not check_password_hash(user.password_hash, password): # takes the hash and cleartext password
             error = 'Incorrect password'
         if error is None:
             login_user(user)
+            flash('Logged in successfully')
             nextp = request.args.get('next') # this gives the url from where the login page was accessed
             print(nextp)
-            if next is None or not nextp.startswith('/'):
-                return redirect(url_for('index'))
+            if nextp is None or not nextp.startswith('/'):
+                return redirect(url_for('main.index'))
             return redirect(nextp)
         else:
             flash(error)
     return render_template('user.html', form=login_form, heading='Login')
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    register_form = RegisterForm()
+
+    if register_form.validate_on_submit():
+        email = register_form.email.data # checks the email isn't already registered
+        existing = db.session.scalar(db.select(User).where(User.email == email))
+        if existing:
+            flash('Email already registered, please log in')
+            return redirect(url_for('auth.register'))
+        
+        pwd_hash = generate_password_hash(register_form.password.data).decode('utf-8') # hashing the password 
+
+        new_user = User(
+            firstname=register_form.firstname.data,
+            surname=register_form.surname.data,
+            email=email,
+            contact_number=register_form.contact_number.data,
+            street_address=register_form.street_address.data,
+            password_hash=pwd_hash
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful, please log in')
+        return redirect(url_for('auth.login'))
+
+    return render_template('user.html', form=register_form, heading='Register')
--- website/forms.py
+++ website/forms.py
@@ -4,18 +4,21 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo
 
 # creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    email = StringField("Email", validators=[InputRequired('Enter email address')])
+    password = PasswordField("Password", validators=[InputRequired('Enter user password')])
     submit = SubmitField("Login")
 
  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    firstname = StringField("First Name", validators=[InputRequired()])
+    surname = StringField("Surname", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[InputRequired(), Email("Please enter a valid email")])
+    contact_number = StringField("Contact Number", validators=[InputRequired()])
+    street_address = StringField("Street Address", validators=[InputRequired()])
     # linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
+    password = PasswordField("Password", validators=[InputRequired(),
                   EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
+    confirm = PasswordField("Confirm Password", validators=[InputRequired()])
 
     # submit button
     submit = SubmitField("Register")
