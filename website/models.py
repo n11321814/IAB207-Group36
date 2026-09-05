8a5a338b82b2363db6077dcccf80696ab31f47d5
Hugo Van Roy <jvanroy01@gmail.com>
2026-09-05 12:40:27 +1000

    Added User model fields for registration

--- website/models.py
+++ website/models.py
@@ -4,6 +4,12 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120),unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    contact_number = db.Column(db.String(20), nullable=False)
+    street_address = db.Column(db.String(200), nullable=False)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +19,5 @@ class Comment(db.Model):
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+
+    
